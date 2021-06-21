04d1ac3991f04cf9ac931773dd680584b8c5611a
gofr <32750931+gofr@users.noreply.github.com>
2021-06-21 19:53:58 +0200

    Add a few more steps to the UI tests

--- cypress/integration/ui_spec.js
+++ cypress/integration/ui_spec.js
@@ -7,13 +7,18 @@ describe('Home page', () => {
         cy.contains('My Boards')
         cy.get('[data-cy=board-item]').should('not.exist')
     })
-    it('can create a board without being logged in', () => {
+    it('can create tasks without being logged in', () => {
         const title = 'Board Name'
         cy.get('[data-cy=create-board]').type(`${title}{enter}`)
         cy.url().should('contain', '/board/')
         cy.get('[data-cy=board-title]').should('have.value', title)
+        cy.get('[data-cy=add-list]').type('List')
+        cy.get('[data-cy=save]').click()
+        cy.get('[data-cy=new-task]').click()
+        cy.get('[data-cy=task-input]').type('A new task{enter}')
+        cy.get('[data-cy=tasks-list]').children().should('have.length', 1)
     })
-    it('can sign up', () => {
+    it('can sign up and log out', () => {
         const username = 'username@example.com'
         cy.get('[data-cy=login-menu]').click()
         cy.contains('Sign up').click()
@@ -21,6 +26,9 @@ describe('Home page', () => {
         cy.get('[data-cy=signup-password]').type('password')
         cy.get('[data-cy=signup]').click()
         cy.get('[data-cy=logged-user]').contains(username)
+        cy.get('[data-cy=logged-user]').click()
+        cy.contains('Log out').click()
+        cy.get('[data-cy=logged-user]').should('not.be.visible')
     })
 })
 
